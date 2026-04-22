423df302669f194d7811d1557d7634027d3772d3
AllaboutCare2026 <lilakatze82@outlook.de>
2026-04-22 18:41:30 +0200

    Update board.js

--- api/board.js
+++ api/board.js
@@ -1,1 +1,11 @@
-
+export default async function handler(req, res) {
+  res.status(200).json({
+    posts: [
+      {
+        title: "Testbeitrag",
+        text: "board.js funktioniert jetzt.",
+        url: ""
+      }
+    ]
+  });
+}
